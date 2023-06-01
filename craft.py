1b826173d278cebb8a0ffd08abc68b7142721258
teddy <79746587+wofvh@users.noreply.github.com>
2023-06-01 18:56:25 +0900

    Update craft.py

--- craft.py
+++ craft.py
@@ -76,7 +76,7 @@ class CRAFT(nn.Module):
         y = self.conv_cls(feature)
 
         return y.permute(0,2,3,1), feature
-
+##go
 if __name__ == '__main__':
     model = CRAFT(pretrained=True).cuda()
     output, _ = model(torch.randn(1, 3, 768, 768).cuda())
